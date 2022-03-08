dbc3f8fb4810f207257c0018bfde413f54af2dfc
SeritiAymen <7Ab0000za>
2022-03-08 13:44:13 +0100

    m

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bdc',
+    'bdc.apps.BdcConfig',
     'corsheaders',
     'rest_framework',
 ]
@@ -126,6 +126,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
